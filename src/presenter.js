15b68fadb76e58e387185101edaf0b19221297b6
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2025-09-09 02:47:53 -0400

    feat: Pide y muestra hora de salida

--- src/presenter.js
+++ src/presenter.js
@@ -2,14 +2,14 @@ import sumar from "./sumador";
 
 const first = document.querySelector("#entrada");
 
-
+const second =document.querySelector("#salida")
 const form = document.querySelector("#peajes-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entrada = new Date(first.value);
-  
+  const salida = new Date(second.value);
 
-  div.innerHTML = "<p>Entrada:" + entrada + "</p> ";
+  div.innerHTML = "<p>Entrada: " + entrada + "</p> Salida: "+salida;
 });
